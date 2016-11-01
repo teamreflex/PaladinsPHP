158146b526073c2f1ac6d4f40353c0a94b511c66
Kyle <kairu@team-reflex.com>
2016-11-01 14:36:23 +1300

    Fix platform URLs

--- src/Platform.php
+++ src/Platform.php
@@ -4,7 +4,7 @@ namespace Reflex\Smite;
 
 abstract class Platform
 {
-    const PC = 'http://api.smitegame.com/smiteapi.svc';
-    const XBOX = 'http://api.xbox.smitegame.com/smiteapi.svc';
-    const PS = 'http://api.ps4.smitegame.com/smiteapi.svc';
+    const PC = 'http://api.smitegame.com/smiteapi.svc/';
+    const XBOX = 'http://api.xbox.smitegame.com/smiteapi.svc/';
+    const PS = 'http://api.ps4.smitegame.com/smiteapi.svc/';
 }
