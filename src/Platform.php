3fc1d831bdd6801a42488297b6bacc7e249a3501
Kyle <kairu@team-reflex.com>
2017-06-28 10:53:08 +1200

    Add Xbox and PS4 platforms

--- src/Platform.php
+++ src/Platform.php
@@ -5,4 +5,6 @@ namespace Reflex\Paladins;
 abstract class Platform
 {
     const PC = 'http://api.paladins.com/paladinsapi.svc/';
+    const XBOX = 'http://api.xbox.paladins.com/paladinsapi.svc/';
+    const PS4 = 'http://api.ps4.paladins.com/paladinsapi.svc/';
 }
